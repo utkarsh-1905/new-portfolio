8e6b5e7f151d2110cab6bac6afe6d5304613235e
Utkarsh <utripathi2002@gmail.com>
2022-08-14 22:57:17 +0530

    responsive navbar

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+    function openDrawer(){
+        const drawer: any = document.querySelector('.invert');
+        drawer.style.height = "60%"
+    }
+    function closeDrawer(){
+        const drawer: any = document.querySelector('.invert');
+        drawer.style.height = "0%"
+    }
+</script>
+
 <nav>
     <ul class="normal">
         <li><a href="/resume">resume</a></li>
@@ -6,11 +17,11 @@
         <li><a href="/contact">contact</a></li>
         <li><a href="/blogs">blogs</a></li>
     </ul>
-    <img class="hamburger" src="/hamburger.svg" width="32px" alt="hamburger menu"/>
+    <img class="hamburger" src="/hamburger.svg" on:click={openDrawer} width="32px" alt="hamburger menu"/>
 </nav>
 <div class="toggleMenu">
     <div class="invert">
-        <img src="/shortDown.svg" class="close" width="32px" alt="close"/>
+        <img src="/shortDown.svg" on:click={closeDrawer} class="close" width="32px" alt="close"/>
         <ul class="responsive">
             <li><a href="/resume">resume</a></li>
             <li><a href="/projects">projects</a></li>
@@ -79,7 +90,7 @@
             margin-top: 0.5rem;
         }
 
-        .hamburger:hover{
+        .hamburger:hover,.close:hover{
             border-radius: 50px;
             background: #eae1e1;
             box-shadow: inset 10px 10px 20px #bbb4b4,inset -10px -10px 20px #ffffff;
@@ -97,14 +108,13 @@
 
         .invert{
             width: 100%;
-            height: 60%;
+            height: 0%;
             background-color: #eae1e1;
             border-radius: 50px;
             box-shadow: 0px -10px 50px 20px rgba(0, 0, 0, 0.3);
-            /* display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            align-items: center; */
+            transition-property: height;
+            transition-duration: 0.3s;
+            transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
         }
 
         .responsive{
@@ -125,6 +135,9 @@
             display:block;
             text-align: center;
             margin: 1rem auto;
+            border-radius: 50px;
+            background: #eae1e1;
+            box-shadow:  10px 10px 20px #a49e9e,-10px -10px 20px #ffffff;
         }
     }
 </style>
