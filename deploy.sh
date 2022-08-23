5a94a161c00da6726d06683d74075f261d929b77
Utkarsh <utripathi2002@gmail.com>
2022-08-23 22:28:19 +0530

    test deploy script added

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
-cd new_portfolio
-git pull
-docker stop portfolio
-docker rm portfolio
-docker rmi $(docker images -f=reference="portfolio" -q)
-docker build . -t portfolio:latest
-docker rmi $(docker images -f="dangling=true" -q)
-docker run -d -p 3000:3000 --name portfolio portfolio:latest
+# cd new_portfolio
+# git pull
+# docker stop portfolio
+# docker rm portfolio
+# docker rmi $(docker images -f=reference="portfolio" -q)
+# docker build . -t portfolio:latest
+# docker rmi $(docker images -f="dangling=true" -q)
+# docker run -d -p 3000:3000 --name portfolio portfolio:latest
+
+ls
+pwd
+curl ifconfig.me
+docker images
+docker ps -a
