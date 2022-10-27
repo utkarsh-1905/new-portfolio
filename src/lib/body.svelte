a1aac924214ad51733097496e950f90d134d044a
Utkarsh <utripathi2002@gmail.com>
2022-10-28 00:13:24 +0530

    started work on landing page

--- src/lib/body.svelte
+++ src/lib/body.svelte
@@ -1,18 +1,66 @@
+<script lang="ts">
+    export const prerender = true;
+    import { onMount } from "svelte";
+    import animationData from "./landing-lottie.json";
+    import lottie from "lottie-web";
+    let lottieContainer: HTMLElement;
+    import Typewriter from "svelte-typewriter";
+
+    onMount(()=>{
+        lottie.loadAnimation({
+            container: lottieContainer,
+            animationData,
+        });
+    });
+    
+    function typewriterDone(){
+        
+    }
+</script>
+
 <div class="container">
-    <div class="heroText">hello people</div>
+    <Typewriter mode="cascade" interval={100} cursor={true} keepCursorOnFinish={true} on:done={typewriterDone}>
+        <h1>hi, i am utkarsh</h1>
+    </Typewriter>
+    <div class="bg-container">
+        <div class="bg" bind:this={lottieContainer}></div>
+    </div>
+
 </div>
 
 <style>
-    .heroText{
-        font-weight: 900;
-        font-size: xx-large;
-    }
     .container{
         margin-top:6rem;
         width:100%;
         display: flex;
         flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        position: relative;
+    }
+    .bg-container{
+        width: 100%;
+        height: 250px;
+        display: flex;
         justify-content: center;
         align-items: center;
+        flex-direction: row;
+        position: relative;
+    }
+    .bg{
+       z-index: -10;
+       /* width: 40%; */
+       position: absolute;
+    }
+
+    @media screen and (max-width:1024px){
+        .bg{
+            width: 60vw;
+        }
+    }
+    @media screen and (max-width:550px){
+        .bg{
+            width: 75vw;
+        }
     }
 </style>
