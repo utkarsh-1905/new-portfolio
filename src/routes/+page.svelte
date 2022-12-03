293e41737bf2b5867110166dd999abd75bd68430
Utkarsh <utripathi2002@gmail.com>
2022-12-04 01:09:14 +0530

    node version changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script lang='ts'>
     import Body from "$lib/body.svelte"
-    import {page} from "$app/stores";
-
-    if($page.error){
-        location.reload();
-    }
 </script>
 
 <div class="container">
